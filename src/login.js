2bfeb1a90e913b0874ea0240eabbb42c86f96d52
jonathankerth <jonathanpkerth@gmail.com>
2023-09-26 14:28:18 -0700

    improve styling

--- src/login.js
+++ src/login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Login.css";
 
 const Login = ({ setIsLoggedIn, darkMode, setDarkMode }) => {
@@ -12,57 +11,59 @@ const Login = ({ setIsLoggedIn, darkMode, setDarkMode }) => {
 
 	const handleLogin = async () => {
 		const auth = getAuth();
-
 		try {
 			await signInWithEmailAndPassword(auth, email, password);
-
 			if (typeof setIsLoggedIn === "function") {
 				setIsLoggedIn(true);
 			}
-
-			navigate("/"); // Navigate to root path
+			navigate("/");
 		} catch (error) {
-			setError(error.message); // Display Firebase error message
+			setError(error.message);
 		}
 	};
 
 	return (
 		<div
-			className={`container mt-5 login-container ${
+			className={`container login-container ${
 				darkMode ? "dark-mode-box" : "light-mode-box"
 			}`}
 		>
-			<h1 className={`text-center ${darkMode ? "text-white" : "text-dark"}`}>
-				Login
-			</h1>
-			<div className="form-group">
-				<input
-					type="email"
-					className="form-control"
-					placeholder="Email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
+			<div className="form-container">
+				<h1 className={`login-header ${darkMode ? "dark-mode-text" : ""}`}>
+					Login
+				</h1>
+				<div className="form-group">
+					<input
+						type="email"
+						className={`form-control ${darkMode ? "dark-mode-input" : ""}`}
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						className={`form-control ${darkMode ? "dark-mode-input" : ""}`}
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<button className="btn btn-primary login-button" onClick={handleLogin}>
+					Login
+				</button>
+				{error && <div className="alert alert-danger mt-3">{error}</div>}
+				<div className="signup-link">
+					Don't have an account? <Link to="/signup">Sign up</Link> instead.
+				</div>
+				<button
+					className="btn btn-secondary mode-toggle-button"
+					onClick={() => setDarkMode(!darkMode)}
+				>
+					{darkMode ? "Light Mode" : "Dark Mode"}
+				</button>
 			</div>
-			<div className="form-group">
-				<input
-					type="password"
-					className="form-control"
-					placeholder="Password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-			</div>
-			<button className="btn btn-primary" onClick={handleLogin}>
-				Login
-			</button>
-			{error && <div className="alert alert-danger mt-3">{error}</div>}
-			<div className={`mt-3 ${darkMode ? "text-white" : "text-dark"}`}>
-				Don't have an account? <Link to="/signup">Sign up</Link> instead.
-			</div>
-			<button onClick={() => setDarkMode(!darkMode)}>
-				{darkMode ? "Light Mode" : "Dark Mode"}
-			</button>
 		</div>
 	);
 };
--- src/signup.js
+++ src/signup.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Login.css"; // Import the same CSS file
+import "./Login.css";
 
 const Signup = ({ darkMode, setDarkMode }) => {
 	const [email, setEmail] = useState("");
@@ -12,52 +11,56 @@ const Signup = ({ darkMode, setDarkMode }) => {
 
 	const handleSignup = async () => {
 		const auth = getAuth();
-
 		try {
 			await createUserWithEmailAndPassword(auth, email, password);
-			navigate("/"); // Navigate to home (or any other path where the user's dashboard is)
+			navigate("/");
 		} catch (error) {
-			setError(error.message); // Display Firebase error message
+			setError(error.message);
 		}
 	};
 
 	return (
 		<div
-			className={`container mt-5 login-container ${
+			className={`container login-container ${
 				darkMode ? "dark-mode-box" : "light-mode-box"
 			}`}
 		>
-			<h1 className={`text-center ${darkMode ? "text-white" : "text-dark"}`}>
-				Sign Up
-			</h1>
-			<div className="form-group">
-				<input
-					type="email"
-					className="form-control"
-					placeholder="Email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
+			<div className="form-container">
+				<h1 className={`login-header ${darkMode ? "dark-mode-text" : ""}`}>
+					Sign Up
+				</h1>
+				<div className="form-group">
+					<input
+						type="email"
+						className={`form-control ${darkMode ? "dark-mode-input" : ""}`}
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						className={`form-control ${darkMode ? "dark-mode-input" : ""}`}
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+				<button className="btn btn-primary login-button" onClick={handleSignup}>
+					Sign Up
+				</button>
+				{error && <div className="alert alert-danger mt-3">{error}</div>}
+				<div className="signup-link">
+					Already have an account? <Link to="/login">Login</Link> instead.
+				</div>
+				<button
+					className="btn btn-secondary mode-toggle-button"
+					onClick={() => setDarkMode(!darkMode)}
+				>
+					{darkMode ? "Light Mode" : "Dark Mode"}
+				</button>
 			</div>
-			<div className="form-group">
-				<input
-					type="password"
-					className="form-control"
-					placeholder="Password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-			</div>
-			<button className="btn btn-primary" onClick={handleSignup}>
-				Sign Up
-			</button>
-			{error && <div className="alert alert-danger mt-3">{error}</div>}
-			<div className={`mt-3 ${darkMode ? "text-white" : "text-dark"}`}>
-				Already have an account? <Link to="/login">Login</Link> instead.
-			</div>
-			<button onClick={() => setDarkMode(!darkMode)}>
-				{darkMode ? "Light Mode" : "Dark Mode"}
-			</button>
 		</div>
 	);
 };
